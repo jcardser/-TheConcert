c68a0e996b8104284f3440314815b924052a729b
JUAN SEBASTIAN CARDONA SERNA <jcardser@gmail.com>
2022-04-22 01:51:28 -0500

    Point

--- TheConcert/TheConcert/Data/SeedDb.cs
+++ TheConcert/TheConcert/Data/SeedDb.cs
@@ -18,17 +18,30 @@ namespace TheConcert.Data
             await CheckTicketsAsync();
         }
 
-        private Task CheckTicketsAsync()
+        private async Task CheckTicketsAsync()
         {
-            throw new NotImplementedException();
+            if (!_context.Tickets.Any())
+            {
+                for(int i=1; i<=5000;i++)
+                {
+                    _context.Tickets.Add(new Ticket { Id = i + 1 });
+                    await _context.SaveChangesAsync();
+                }
+                
+            }
         }
 
         private async Task CheckEntrancesAsync()
         {
-            _context.Entrances.Add(new Entrance { Description = "Norte" });
-            _context.Entrances.Add(new Entrance { Description = "Sur" });
-            _context.Entrances.Add(new Entrance { Description = "Oriental" });
-            _context.Entrances.Add(new Entrance { Description = "Occidental" });
+            if(!_context.Entrances.Any())
+            {
+                _context.Entrances.Add(new Entrance { Description = "Norte" });
+                _context.Entrances.Add(new Entrance { Description = "Sur" });
+                _context.Entrances.Add(new Entrance { Description = "Oriental" });
+                _context.Entrances.Add(new Entrance { Description = "Occidental" });
+                await _context.SaveChangesAsync();
+            }
+
         }
 
 
